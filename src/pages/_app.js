6722d7c8a2fff7faf66eda2886796f7a2980d3e2
alex t <amojarrot@gmail.com>
2022-06-29 11:06:05 -0600

    moralis auth working
    added prettier
    
    changed moralis into new page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl='https://rsrqtfhqsfhm.usemoralis.com:2053/server'
+      appId='VoMFmmbGFfVC3GEOPQxOZvJctC6VYMDwyIu44ba0'>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
