c52c14a58bac29a45a37dcfeb95ebe74801c6397
alex t <amojarrot@gmail.com>
2022-07-04 09:45:05 -0600

    callback oauth working

--- src/pages/api/callback.js
+++ src/pages/api/callback.js
@@ -1,3 +1,14 @@
-export default function handler(req, res) {
-  res.send('hello world')
+import { oauth2Client } from './loginyt'
+
+export default async function handler(req, res) {
+  const code = req.query.code
+
+  if (!code) return res.send('unauthenticated')
+
+  const { tokens } = await oauth2Client.getToken(code)
+
+  // do something with tokens
+  // console.log(tokens)
+
+  res.send('authenticated')
 }
