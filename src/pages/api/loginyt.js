9b559bd504a1677a22a137b2c52483c5f44c300c
dmojarrot <a01638460@itesm.mx>
2022-07-04 16:09:47 -0500

    Revert "callback oauth working"
    This reverts commit c52c14a58bac29a45a37dcfeb95ebe74801c6397.

--- src/pages/api/loginyt.js
+++ src/pages/api/loginyt.js
@@ -1,14 +1,14 @@
 import { google } from 'googleapis'
 
 // this credentials are secret
-// TODO: move to .env and export to its own module (importing in callback)
-export const oauth2Client = new google.auth.OAuth2(
+// TODO: move to .env
+const oauth2Client = new google.auth.OAuth2(
   // clientId
   '1080843365185-5jvbk1upbvh5j3b1itsjecbn2l7khhga.apps.googleusercontent.com',
   // clientSecret
   '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',
   // callback url
-  'http://localhost:3000/api/callback'
+  'http://localhost:3000/callback'
 )
 
 const authorizationUrl = oauth2Client.generateAuthUrl({
