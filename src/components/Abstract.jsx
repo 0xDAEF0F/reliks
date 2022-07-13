2cd46331bb8f2c9698d23b0a199bb959e7973131
Alex T <aletapia@proton.me>
2022-07-13 09:54:16 -0600

    added abstract draft

--- src/components/Abstract.jsx
+++ src/components/Abstract.jsx
@@ -9,15 +9,15 @@ function Abstract() {
             <div className='mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center'>
               <div className='lg:py-24'>
                 <h1 className='mt-4 text-4xl tracking-tight font-extrabold text-mauve dark:text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl'>
-                  <span className='block'>Dolor excepteur</span>
+                  <span className='block'>Unite with</span>
                   <span className='block text-light-violet11 dark:text-darkMode-violet11'>
-                    ut aliquip ut
+                    your favorite creator.
                   </span>
                 </h1>
                 <p className='mt-3 text-base text-light-gray dark:text-darkMode-gray sm:mt-5 sm:text-xl lg:text-lg xl:text-xl'>
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat.
+                  Meldrop is a platform for content creators to connect with
+                  their fan base, get supported, and in return, grant rewards in
+                  the form of NFTs and token drops.
                 </p>
               </div>
             </div>
