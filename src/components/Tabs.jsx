efec459d26e397be9a633fb0e5c1311cf5dacd9f
dmojarrot <a01638460@itesm.mx>
2022-07-18 23:07:32 -0500

    Added join whale lair component

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,191 @@
+import { useState } from 'react'
+import { Tab } from '@headlessui/react'
+import { FaDiscord } from 'react-icons/fa'
+import { MdWaterDrop } from 'react-icons/md'
+import { CgSandClock } from 'react-icons/cg'
+import { GiClothes } from 'react-icons/gi'
+import { FaPlus } from 'react-icons/fa'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const features = [
+  {
+    name: 'Discord server',
+    description:
+      'Nunc a, lacinia sed risus neque, arcu, rhoncus. Id mauris justo facilisis aliquam platea vestibulum condimentum morbi.',
+    icon: FaDiscord,
+  },
+  {
+    name: 'NFT drops',
+    description:
+      'Purus lobortis volutpat posuere id integer nunc tellus. Non mauris malesuada feugiat massa mi pellentesque cum est. Pharetra a varius urna rhoncus, tempor rutrum.',
+    icon: MdWaterDrop,
+  },
+  {
+    name: 'limited-time rewards',
+    description:
+      'Purus lobortis volutpat posuere id integer nunc tellus. Non mauris malesuada feugiat massa mi pellentesque cum est. Pharetra a varius urna rhoncus, tempor rutrum.',
+    icon: CgSandClock,
+  },
+  {
+    name: 'Merch',
+    description:
+      'Tincidunt sollicitudin interdum nunc sit risus at bibendum vitae. Urna, quam ut sit justo non, consectetur et varius.',
+    icon: GiClothes,
+  },
+]
+// const checklist = [
+//   'Unlimited projects',
+//   'No per user fees',
+//   'Unlimited storage',
+//   '24/7 support',
+//   'Cancel any time',
+//   '14 days free',
+// ]
+
+function Strategies() {
+  return (
+    <div>
+      <div className='mx-10 '>
+        <div className='relative'>
+          <div className='absolute inset-0' aria-hidden='true'>
+            <div className='inset-y-0 right-0 w-1/2 bg-gradient-to-r from-light-violet8 to-light-violet9 dark:from-darkMode-violet8 dark:to-darkMode-violet9 lg:absolute' />
+          </div>
+          <div className='relative mx-auto max-w-7xl lg:grid lg:grid-cols-2 lg:px-8'>
+            <div className='px-4 sm:px-6 lg:px-0 lg:pr-8'>
+              <div className='mx-auto max-w-lg lg:mx-0'>
+                <h2 className='text-base font-medium uppercase tracking-wide text-light-violet11 dark:text-darkMode-violet11'>
+                  Whale Strategy
+                </h2>
+                <p className='text-gray-900 mt-2 text-2xl font-semibold sm:text-3xl'>
+                  Join Diego Mojarro lair
+                </p>
+                <dl className='mt-12 space-y-10'>
+                  {features.map((feature) => (
+                    <div key={feature.name} className='relative'>
+                      <dt>
+                        <div className='absolute flex h-12 w-12 items-center justify-center  rounded-lg bg-gradient-to-r from-light-violet8 to-light-violet9 object-cover dark:from-darkMode-violet8 dark:to-darkMode-violet9 '>
+                          <feature.icon
+                            className='h-6 w-6 text-white'
+                            aria-hidden='true'
+                          />
+                        </div>
+                        <p className='ml-16 text-lg font-medium leading-6 text-darkMode-violet10'>
+                          {feature.name}
+                        </p>
+                      </dt>
+                      <dd className='mt-2 ml-16 text-base text-light-gray opacity-75 dark:text-darkMode-gray'>
+                        {feature.description}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+            <div className='lg:flex lg:items-center lg:justify-end  lg:px-0 lg:pl-8'>
+              <div className='mx-auto mt-8 w-full max-w-lg space-y-8 rounded-lg bg-gradient-to-r from-light-violet8 to-light-violet9 p-4 dark:from-darkMode-violet8 dark:to-darkMode-violet9 lg:mx-0 lg:bg-none'>
+                <div>
+                  <h2 className='sr-only'>Price</h2>
+                  <p className='relative grid grid-cols-2'>
+                    <span className='flex flex-col text-center'>
+                      <span className='text-5xl font-extrabold tracking-tight text-white'>
+                        $99
+                      </span>
+                      <span className='text-cyan-100 mt-2 text-base font-medium'>
+                        Per month
+                      </span>
+                      <span className='sr-only'>plus</span>
+                    </span>
+                    <span
+                      className='pointer-events-none absolute flex h-12 w-full items-center justify-center'
+                      aria-hidden='true'>
+                      <FaPlus className='h-6 w-6 text-white' aria-hidden='true' />
+                    </span>
+                    <span>
+                      <span className='flex flex-col text-center'>
+                        <span className='text-5xl font-extrabold tracking-tight text-white'>
+                          $4
+                        </span>
+                        <span className='text-cyan-100 mt-2 text-base font-medium'>
+                          Marketplace fee
+                        </span>
+                      </span>
+                    </span>
+                  </p>
+                </div>
+                {/* <ul
+                role='list'
+                className='grid gap-px overflow-hidden rounded sm:grid-cols-2'>
+                {checklist.map((item) => (
+                  <li
+                    key={item}
+                    className='bg-cyan-700 flex items-center bg-opacity-50 py-4 px-4 text-base text-white'>
+                    <CheckIcon className='text-cyan-200 h-6 w-6' aria-hidden='true' />
+                    <span className='ml-3'>{item}</span>
+                  </li>
+                ))}
+              </ul> */}
+                <a
+                  href='#'
+                  className='flex w-full  items-center justify-center rounded-md bg-mauve py-4 px-8 text-lg font-medium leading-6 text-light-violet1 hover:opacity-80 dark:bg-white dark:text-darkMode-violet1 md:px-10'>
+                  Mint contract
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function OwnedNft() {
+  return <div>2</div>
+}
+
+function Activity() {
+  return <div>3</div>
+}
+
+export default function Tabs() {
+  let [categories] = useState({
+    Strategies: Strategies(),
+    "Nft's": OwnedNft(),
+    Activity: Activity(),
+  })
+
+  return (
+    <div>
+      <Tab.Group>
+        <div className=''>
+          <Tab.List className='bg-gray-200 flex w-full space-x-1 rounded-xl p-1 '>
+            {Object.keys(categories).map((category) => (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    'w-full py-2.5 text-sm font-medium leading-5',
+                    'focus:outline-none ',
+                    selected
+                      ? 'border-b-4 border-darkMode-violet6'
+                      : 'hover:bg-light-violet2 hover:brightness-90 dark:hover:bg-darkMode-violet2 dark:hover:brightness-75'
+                  )
+                }>
+                {category}
+              </Tab>
+            ))}
+          </Tab.List>
+        </div>
+        <Tab.Panels>
+          {Object.values(categories).map((category, idx) => (
+            <Tab.Panel className='mt-10 focus:outline-none' key={idx}>
+              {category}
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  )
+}
--- src/pages/profile/[username].js
+++ src/pages/profile/[username].js
@@ -5,6 +5,7 @@ import SignUpAsCreatorBanner from '../../components/SignUpAsCreatorBanner'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import ModalWhaleContract from '../../components/ModalWhaleContract'
+import Tabs from '../../components/Tabs'
 import toast from 'react-hot-toast'
 import { TiSocialYoutubeCircular } from 'react-icons/ti'
 import Link from 'next/link'
@@ -35,73 +36,79 @@ function Profile() {
     <>
       <Header />
       <SignUpAsCreatorBanner />
-      <div className='mx-auto max-w-6xl pt-20 md:pt-3 '>
-        <div className='block'>
-          <Image
-            src={user.coverPhoto || '/wp.png'}
-            width={1000}
-            height={220}
-            quality={100}
-            className='object-cover object-center md:rounded-t-lg'
-            layout='responsive'
-            alt='banner'
-          />
-        </div>
-        <div className='mx-auto h-full items-start rounded-b-lg bg-light-violet2 pt-4 shadow-sm  dark:bg-darkMode-violet2  md:flex md:justify-between'>
-          <div className='flex flex-col items-center md:mx-10 md:flex-row'>
-            <div className='relative -inset-y-9'>
-              <div className='relative h-24 w-24 rounded-full shadow-md'>
-                <Image
-                  src={user.pfp || '/pp.jpg'}
-                  className='rounded-full object-cover object-center'
-                  quality={100}
-                  layout='fill'
-                  alt='profile picture'
-                />
+      <div className='mx-auto max-w-6xl pt-20 md:pt-0 '>
+        <div className='relative rounded-b-lg bg-light-violet2 shadow-sm dark:bg-darkMode-violet2'>
+          <div className='block'>
+            <Image
+              src={user.coverPhoto || '/wp.png'}
+              width={1000}
+              height={220}
+              quality={100}
+              priority
+              className='object-cover object-center md:rounded-t-lg'
+              layout='responsive'
+              alt='banner'
+            />
+          </div>
+          <div className='mx-auto mb-2 h-full items-start border-b border-dotted border-light-violet6 pt-4 dark:border-darkMode-violet6 md:flex md:justify-between'>
+            <div className='flex flex-col items-center md:mx-10 md:flex-row'>
+              <div className='relative -inset-y-9'>
+                <div className='relative h-24 w-24 rounded-full shadow-md'>
+                  <Image
+                    src={user.pfp || '/pp.jpg'}
+                    className='rounded-full object-cover object-center'
+                    quality={100}
+                    priority
+                    layout='fill'
+                    alt='profile picture'
+                  />
+                </div>
               </div>
-            </div>
-            <div className='ml-0 -mt-9 text-center md:-mt-0 md:ml-3 md:text-left'>
-              <p className='ml-1 text-3xl font-semibold text-light-violet12 dark:text-darkMode-violet12'>
-                {user.channelTitle}
-              </p>
-              <div className='flex flex-col items-center md:flex-row md:items-end'>
-                <button
-                  onClick={copy2Clipboard}
-                  title='Copy'
-                  className='mt-1 h-auto w-36 rounded-full bg-light-violet3 py-1 px-2 hover:bg-light-violet4 active:bg-light-violet5 dark:bg-darkMode-violet3 dark:hover:bg-darkMode-violet4 dark:hover:text-darkMode-gray dark:active:bg-darkMode-violet5 md:mt-1'>
-                  <div className='flex items-center justify-between'>
-                    <div className='relative h-5 w-5'>
-                      <Image src='/ethereum.svg' alt='MetaMask logo' layout='fill' />
+              <div className='ml-0 -mt-9 text-center md:-mt-0 md:ml-3 md:text-left'>
+                <p className='ml-1 text-3xl font-semibold text-light-violet12 dark:text-darkMode-violet12'>
+                  {user.channelTitle}
+                </p>
+                <div className='flex flex-col items-center md:flex-row md:items-end'>
+                  <button
+                    onClick={copy2Clipboard}
+                    title='Copy'
+                    className='mt-1 h-auto w-36 rounded-full bg-light-violet3 py-1 px-2 hover:bg-light-violet4 active:bg-light-violet5 dark:bg-darkMode-violet3 dark:hover:bg-darkMode-violet4 dark:hover:text-darkMode-gray dark:active:bg-darkMode-violet5 md:mt-1'>
+                    <div className='flex items-center justify-between'>
+                      <div className='relative h-5 w-5'>
+                        <Image src='/ethereum.svg' alt='MetaMask logo' layout='fill' />
+                      </div>
+                      <p className='h-5 truncate pl-1 text-left text-sm font-semibold opacity-60'>
+                        {userAddress}
+                      </p>
                     </div>
-                    <p className='h-5 truncate pl-1 text-left text-sm font-semibold opacity-60'>
-                      {userAddress}
-                    </p>
-                  </div>
-                </button>
-                <p className='opacity-85 mt-1 ml-1 text-xs text-light-gray dark:text-darkMode-gray'>
-                  Joined:{' '}
-                  {user &&
-                    new Date(user.createdAt).toLocaleDateString(undefined, options)}
+                  </button>
+                  <p className='opacity-85 mt-1 ml-1 text-xs text-light-gray dark:text-darkMode-gray'>
+                    Joined:{' '}
+                    {user &&
+                      new Date(user.createdAt).toLocaleDateString(undefined, options)}
+                  </p>
+                </div>
+                <p className='mx-auto mt-4 w-2/4 whitespace-nowrap pb-5 md:ml-2'>
+                  {user.bio}
                 </p>
               </div>
-              <p className='mx-auto mt-4 w-2/4 whitespace-nowrap pb-5 md:ml-2'>
-                {user.bio}
-              </p>
+            </div>
+            <div className='mr-2 mb-2 flex justify-end md:mr-10'>
+              {/* <Link href={USER.youtubeCredentials.channelId}> */}
+              <Link href='#'>
+                <a>
+                  <TiSocialYoutubeCircular
+                    size={50}
+                    className='text-light-gray opacity-80 hover:opacity-60 dark:text-darkMode-gray'
+                  />
+                </a>
+              </Link>
+              {/* <ModalWhaleContract /> */}
             </div>
           </div>
-          <div className='mr-2 mb-2 flex justify-end md:mr-10'>
-            <Link href='#'>
-              <a>
-                <TiSocialYoutubeCircular
-                  size={40}
-                  className='text-light-gray opacity-80 hover:opacity-60 dark:text-darkMode-gray'
-                />
-              </a>
-            </Link>
-            {/* <ModalWhaleContract /> */}
-          </div>
+          <Tabs />
+          <br />
         </div>
-        <br />
       </div>
     </>
   )
