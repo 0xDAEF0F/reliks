81d13d15d32bda190ecb9819caac38426d48b183
Alex T <aletapia@proton.me>
2022-08-04 14:54:01 -0600

    added toggle icon light-dark mode and refactored Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
-import { HiOutlineMoon } from 'react-icons/hi'
+import { HiOutlineMoon, HiOutlineSun } from 'react-icons/hi'
+import { AiOutlineSearch } from 'react-icons/ai'
 import { useMoralis } from 'react-moralis'
 import ProfileDropdownMenu from './ProfileDropdownMenu'
 import MobileMenu from './MobileMenu'
 import toast from 'react-hot-toast'
 import to from 'await-to-js'
-import { AiOutlineSearch } from 'react-icons/ai'
 
 function Header() {
   const [top, setTop] = useState(true)
   const { theme, setTheme } = useTheme()
   const { authenticate, isAuthenticated } = useMoralis()
 
-  // detect whether user has scrolled the page down by 10px
   useEffect(() => {
     const scrollHandler = () => {
       window.pageYOffset > 10 ? setTop(false) : setTop(true)
@@ -24,16 +23,15 @@ function Header() {
   }, [top])
 
   const login = async () => {
-    if (isAuthenticated) {
-      toast.success('Already Signed In.')
-      return
-    }
     const [, usr] = await to(
       authenticate({ signingMessage: 'Please Sign Message to Log In.' })
     )
     if (usr) toast.success('Succesfully Signed In.')
     if (!usr) toast.error(`Could not sign in. Please try again.`)
   }
+
+  const ThemeIcon = () =>
+    theme === 'light' ? <HiOutlineMoon size={20} /> : <HiOutlineSun size={20} />
 
   return (
     <header
@@ -69,7 +67,6 @@ function Header() {
           <div className='flex justify-center px-2 lg:justify-end'>
             <div className='lg:block'>
               <ul className='flex flex-grow items-center justify-end'>
-                {/* TEMPORARY LINKS*/}
                 <div className='hidden space-x-14 lg:block'>
                   <Link href='/explore'>
                     <a className='font-semibold text-black opacity-70 transition duration-100 hover:opacity-100 dark:text-white'>
@@ -77,14 +74,13 @@ function Header() {
                     </a>
                   </Link>
                 </div>
-
-                {!isAuthenticated ? (
+                {!isAuthenticated && (
                   <button
                     className='ml-10 hidden font-semibold text-black opacity-70 transition duration-100 hover:opacity-100 dark:text-white lg:block'
                     onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-                    <HiOutlineMoon className='dark:fill-white' size={20} />
+                    <ThemeIcon />
                   </button>
-                ) : null}
+                )}
                 <div className='block lg:hidden'>
                   {/* Mobile menu button */}
                   <MobileMenu />
@@ -92,19 +88,13 @@ function Header() {
                 <div className='ml-10 hidden lg:block '>
                   {/* Profile dropdown */}
                   {isAuthenticated ? (
-                    <div className='flex flex-grow flex-wrap items-center justify-end'>
-                      <ProfileDropdownMenu />
-                    </div>
+                    <ProfileDropdownMenu />
                   ) : (
-                    <ul className='flex flex-grow flex-wrap items-center justify-end'>
-                      <li>
-                        <button
-                          onClick={() => login()}
-                          className='whitespace-nowrap rounded-3xl bg-gradient-to-tr from-light-violet8 via-light-violet10 to-light-violet8 px-5 py-3 text-light-violet1 hover:brightness-90 dark:from-[#4568dc]  dark:to-[#bb46a4] dark:hover:opacity-75'>
-                          Connect Wallet
-                        </button>
-                      </li>
-                    </ul>
+                    <button
+                      onClick={login}
+                      className='whitespace-nowrap rounded-3xl bg-gradient-to-tr from-light-violet8 via-light-violet10 to-light-violet8 px-5 py-3 text-light-violet1 hover:brightness-90 dark:from-[#4568dc]  dark:to-[#bb46a4] dark:hover:opacity-75'>
+                      Connect Wallet
+                    </button>
                   )}
                 </div>
               </ul>
