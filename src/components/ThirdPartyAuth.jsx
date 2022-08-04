f1b2bb53c976ddfa13a8657163a9970f71f88c0a
dmojarrot <a01638460@itesm.mx>
2022-08-04 13:33:44 -0500

    Minor fix

--- src/components/ThirdPartyAuth.jsx
+++ src/components/ThirdPartyAuth.jsx
@@ -10,13 +10,13 @@ export default function SocialAppAuth({ connectYoutube }) {
       <h2 className='text-xl font-medium leading-6 '>Connect your account</h2>
       <button
         onClick={connectYoutube}
-        className='mt-5 flex w-3/4 items-center justify-center rounded-lg bg-[#FF0000] py-2 shadow-md hover:opacity-70 focus:border-light-violet7 focus:outline-none focus:ring-2 focus:ring-light-violet7 focus:ring-offset-2 dark:focus:border-darkMode-violet7 dark:focus:ring-darkMode-violet7'>
+        className='mt-5 flex w-3/4 transform items-center justify-center rounded-lg bg-[#FF0000] py-2 shadow-md transition duration-200 hover:scale-105 focus:outline-none'>
         <ImYoutube2 className='h-6 w-6 text-white' />
         <p className='pl-2 text-xs font-semibold text-white'>Continue with Youtube</p>
       </button>
       <button
         onClick={notSupportedToast}
-        className='mt-5 flex w-3/4 items-center justify-center rounded-lg bg-[#9146FF] py-2 shadow-md hover:brightness-90 focus:border-light-violet7 focus:outline-none focus:ring-2 focus:ring-light-violet7 focus:ring-offset-2 dark:focus:border-darkMode-violet7 dark:focus:ring-darkMode-violet7'>
+        className='mt-5 flex w-3/4 transform items-center justify-center rounded-lg bg-[#9146FF] py-2 shadow-md transition duration-200 hover:scale-105  focus:outline-none '>
         <Image width={20} height={20} src='/twitchLogoWhite.png' alt='twitch' />
         <p className='my-1 pl-2 text-xs font-semibold text-white'>Continue with Twitch</p>
       </button>
